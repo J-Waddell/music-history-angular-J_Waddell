ae7019417d7dd39a3ebd922a2dcd545c5768c01f
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-01-30 18:29:44 -0600

    added song titles to dom just need to add description when btn is pressed

--- main.js
+++ main.js
@@ -1,4 +1,62 @@
 console.log('Taters')
 
 const app = angular.module('musicHistApp', ['ngRoute', 'ngMaterial'])
+    .config(($routeProvider) => {
+        $routeProvider
+            .when('/', {
+                controller: 'RootCtrl',
+                templateUrl: '/partials/songlist.html'
+            })
+            .when('/details', {
+                controller: 'DetCtrl',
+                templateUrl: '/partials/songdetails.html'
+            })
+    })
 
+    .controller('RootCtrl', function($scope, $location, $http) {
+        console.log('RootCtrl')
+        $scope.gotoMusic = () => {
+            $scope.gotoMusic = () => $location.url(`/music/${$scope.music}`)
+        }
+    })
+
+    .factory('musicFactory', ($http) => {
+        return {
+            getMusic : function() {
+                return $http
+                .get('music.json')
+                .then((music) => {
+                    console.log("json", music.data)
+                    return music.data
+                })
+            }
+        }
+    })
+
+    .factory('detailsFactory', ($http) => {
+        return {
+            getDetails : function() {
+                return $http
+                .get('music.json')
+                .then((music) => {
+                    return music.data
+                })
+            }
+        }
+    })
+
+    .controller('RootCtrl', function($scope, $routeParams, musicFactory) {
+        console.log('I am a RootCtrl also')
+        musicFactory.getMusic()
+            .then((value) => {
+                $scope.title = value.music
+            })
+    })
+
+    .controller('DetCtrl', function($scope, $routeParams, detailsFactory) {
+        console.log('I am DetCtrl')
+        detailsFactory.getDetails()
+            .then((description) => {
+                $scope.info = description.music
+            })
+    })
